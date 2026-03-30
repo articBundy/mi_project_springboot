48760880fbbbbe8f9bd911f6c6abef4b53e0fd64
articBundy <ckiba25@gmail.com>
2026-03-30 17:40:00 -0600

    Add files via upload

---
+++ demo/src/main/java/com/factura/InvoiceData.java
@@ -0,0 +1,30 @@
+package com.factura;
+
+public class InvoiceData {
+    private String descripcion;
+    private Double precio;
+
+    public InvoiceData() {
+    }
+
+    public InvoiceData(String descripcion, Double precio) {
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+}
